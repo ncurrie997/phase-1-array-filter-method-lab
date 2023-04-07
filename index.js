d08392462975d2a745318c2399f5cdf76ea4de62
Nick Currie <gosox997@gmail.com>
2023-04-07 11:21:38 -0400

    all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(array, query) {
+    return array.filter((element) => element.toLowerCase().includes(query.toLowerCase()));
+}
+
+function fuzzyMatch(array, query) {
+    return array.filter((element) => element.substring(0, 2).toLowerCase().includes(query.toLowerCase()));
+}
+
+function matchName(array, query) {
+    let filtered = array.filter(arr => arr.name && arr.name === query)
+    return filtered;
+  }
